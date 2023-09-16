65dbcc1ca4dd357735d160f6fc14024805f29412
Blazmian <demionfeick@gmail.com>
2023-09-16 11:36:33 -0700

    Solved Layout Issues

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import { Container } from 'react-bootstrap'
 
 const ClusterFooter = () => {
     return (
-        <Container style={{ textAlign: 'center', backgroundColor: '#464646', color: 'white' }} fluid>
+        <Container style={{ textAlign: 'center', backgroundColor: '#464646', color: 'white', }} fluid>
             Copyright © 2023 Cluster Minero de Sonora
         </Container>
     )
